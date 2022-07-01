b9eb53ef0b14e01bad674c84bf1870c05bdc61a9
noelkips <noelkipronolangat@gmail.com>
2022-07-01 14:44:55 +0300

    Update urls.py

--- class_attendance_system/urls.py
+++ class_attendance_system/urls.py
@@ -18,5 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('attendance.urls')),
+    path('', include('users.urls')),
+    path('', include('django.contrib.auth.urls')),
+    path('attendance/', include('attendance.urls')),
 ]
